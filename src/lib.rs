586bdfd0c42409023c00b82f989e51ef57975092
Alex Crichton <alex@alexcrichton.com>
2014-12-11 19:57:07 -0800

    Add the root url to the docs

--- src/lib.rs
+++ src/lib.rs
@@ -56,7 +56,8 @@
 #![crate_type = "rlib"]
 #![crate_type = "dylib"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
-       html_favicon_url = "http://www.rust-lang.org/favicon.ico")]
+       html_favicon_url = "http://www.rust-lang.org/favicon.ico",
+       html_root_url = "http://doc.rust-lang.org/uuid/")]
 
 #![feature(default_type_params)]
 
