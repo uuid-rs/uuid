0cee05a22c68868b9fd758e5bac05c3198537a1c
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-02-02 13:29:33 -0800

    doc(link): fix documentation links
    Now routes to docs.rs instead of doc.rust-lang.org
    
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -100,7 +100,7 @@
 
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-       html_root_url = "https://doc.rust-lang.org/uuid/")]
+       html_root_url = "https://docs.rs/uuid")]
 
 #![deny(warnings)]
 #![no_std]
