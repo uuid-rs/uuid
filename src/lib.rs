88c4e80979926eb32b0d217f9243ca02a260f245
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-02-03 19:34:37 -0800

    refactor(std): std not libstd
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -26,13 +26,10 @@
 //!
 //! # Dependencies
 //!
-//! By default, this crate depends on nothing but `libstd` and cannot generate
+//! By default, this crate depends on nothing but `std` and cannot generate
 //! [`Uuid`]s. You need to enable the following Cargo features to enable
 //! various pieces of functionality:
 //!
-//! * `std` - adds in functionality available when linking to the standard
-//!   library, currently this is only the `impl Error for ParseError`. This is
-//!   enabled by default.
 //! * `v1` - adds the `Uuid::new_v1` function and the ability to create a V1
 //!   using a `UUIDV1Context` and a timestamp from `time::timespec`
 //! * `v3` - adds the `Uuid::new_v3` function and the ability to create a V3
