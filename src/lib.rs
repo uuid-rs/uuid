f3b37e638c4fd8665403c4e84e18498ac7bf3c5e
dylan_DPC <dylan.dpc@gmail.com>
2018-03-17 21:22:23 +0530

    feature check added

--- src/lib.rs
+++ src/lib.rs
@@ -705,6 +705,7 @@ impl Uuid {
     /// assert_eq!(expected_uuid, uuid);
     /// ```
     ///
+    #[cfg(feature = "v4")]
     #[allow(dead_code)]
     fn from_rng_bytes(b: [u8; 16]) -> Uuid {
         let mut uuid = Uuid { bytes: [0; 16] };
@@ -1826,7 +1827,8 @@ mod tests {
     }
 
     #[test]
-    fn test_from_rnd_bytes(){
+    #[cfg(feature = "v4")]
+    fn test_from_rng_bytes(){
         let b = [
             0xa1, 0xa2, 0xa3, 0xa4, 0xb1, 0xb2, 0xc1, 0xc2, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6,
             0xd7, 0xd8,
