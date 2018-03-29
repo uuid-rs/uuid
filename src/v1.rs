8d17b0c6bf268f208ac832a9187ab6741b97c535
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-03-28 19:26:56 -0700

    reorganise Uuid::new_v1
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/v1.rs
+++ src/v1.rs
@@ -22,68 +22,99 @@ pub trait UuidClockSequence {
     fn generate_sequence(&self, seconds: u64, nano_seconds: u32) -> u16;
 }
 
-/// The number of 100 ns ticks between the UUID epoch `1582-10-15 00:00:00` and
-/// the Unix epoch `1970-01-01 00:00:00`.
-const UUID_TICKS_BETWEEN_EPOCHS: u64 = 0x01B2_1DD2_1381_4000;
-
 impl Uuid {
-    /// Creates a new `Uuid` (version 1 style) using a time value + seq + NodeID.
+    /// Create a new [`Uuid`] (version 1) using a time value + sequence +
+    /// *NodeId*.
     ///
     /// This expects two values representing a monotonically increasing value
-    /// as well as a unique 6 byte NodeId, and an implementation of `UuidV1ClockSequence`.
-    /// This function is only guaranteed to produce unique values if the following conditions hold:
+    /// as well as a unique 6 byte NodeId, and an implementation of
+    /// [`UuidClockSequence`]. This function is only guaranteed to produce
+    /// unique values if the following conditions hold:
     ///
-    /// 1. The NodeID is unique for this process,
-    /// 2. The Context is shared across all threads which are generating V1 UUIDs,
-    /// 3. The `UuidV1ClockSequence` implementation reliably returns unique clock sequences
-    ///    (this crate provides `UuidV1Context` for this purpose).
+    /// 1. The *NodeId* is unique for this process,
+    /// 2. The *Context* is shared across all threads which are generating v1
+    ///    [`Uuid`]s,
+    /// 3. The [`UuidClockSequence`] implementation reliably returns unique
+    ///    clock sequences (this crate provides [`UuidV1Context`] for this
+    ///    purpose. However you can create your own [`UuidClockSequence`]
+    ///    implementation, if [`UuidContext`] does not meet your needs).
     ///
-    /// The NodeID must be exactly 6 bytes long. If the NodeID is not a valid length
-    /// this will return a `ParseError::InvalidLength`.
+    /// The NodeID must be exactly 6 bytes long. If the NodeID is not a valid
+    /// length this will return a [`ParseError`]`::InvalidLength`.
     ///
-    /// The function is not guaranteed to produce monotonically increasing values
-    /// however.  There is a slight possibility that two successive equal time values
-    /// could be supplied and the sequence counter wraps back over to 0.
+    /// The function is not guaranteed to produce monotonically increasing
+    /// values however.  There is a slight possibility that two successive
+    /// equal time values could be supplied and the sequence counter wraps back
+    /// over to 0.
     ///
-    /// If uniqueness and monotonicity is required, the user is responsibile for ensuring
-    /// that the time value always increases between calls
-    /// (including between restarts of the process and device).
+    /// If uniqueness and monotonicity is required, the user is responsible for
+    /// ensuring that the time value always increases between calls (including
+    /// between restarts of the process and device).
     ///
     /// Note that usage of this method requires the `v1` feature of this crate
     /// to be enabled.
     ///
     /// # Examples
+    ///
     /// Basic usage:
     ///
-    /// ```
-    /// use uuid::{Uuid, UuidV1Context};
+    /// ```rust
+    /// use uuid::Uuid;
+    /// use uuid::v1::UuidContext;
     ///
-    /// let ctx = UuidV1Context::new(42);
-    /// let v1uuid = Uuid::new_v1(&ctx, 1497624119, 1234, &[1,2,3,4,5,6]).unwrap();
-    ///
-    /// assert_eq!(v1uuid.hyphenated().to_string(), "f3b4958c-52a1-11e7-802a-010203040506");
+    /// let context = UuidContext::new(42);
+    /// if let Ok(uuid) = Uuid::new_v1(&context, 1497624119, 1234, &[1,2,3,4,5,6]) {
+    ///     assert_eq!(uuid.hyphenated().to_string(), "f3b4958c-52a1-11e7-802a-010203040506")
+    /// } else {
+    ///     panic!()
+    /// }
     /// ```
-    pub fn new_v1<C: UuidClockSequence>(
-        context: &C,
+    ///
+    /// [`ParseError`]: ../enum.ParseError.html
+    /// [`Uuid`]: ../struct.Uuid.html
+    /// [`UuidClockSequence`]: struct.UuidClockSequence.html
+    /// [`UuidContext`]: struct.UuidContext.html
+    pub fn new_v1<T>(
+        context: &T,
         seconds: u64,
         nano_seconds: u32,
-        node: &[u8],
-    ) -> Result<Self, ParseError> {
-        if node.len() != 6 {
-            return Err(ParseError::InvalidLength(node.len()))
+        node_id: &[u8],
+    ) -> Result<Self, ParseError>
+    where
+        T: UuidClockSequence,
+    {
+        if node_id.len() != 6 {
+            return Err(ParseError::InvalidLength(node_id.len()));
         }
 
-        let count = context.generate_sequence(seconds, nano_seconds);
-        let timestamp = seconds * 10_000_000 + u64::from(nano_seconds / 100);
-        let uuid_time = timestamp + UUID_TICKS_BETWEEN_EPOCHS;
-        let time_low: u32 = (uuid_time & 0xFFFF_FFFF) as u32;
-        let time_mid: u16 = ((uuid_time >> 32) & 0xFFFF) as u16;
-        let time_high_and_ver: u16 = (((uuid_time >> 48) & 0x0FFF) as u16) | (1 << 12);
-        let mut d4 =[0_u8; 8];
-        d4[0] = (((count & 0x3F00) >> 8) as u8) | 0x80;
-        d4[1] = (count & 0xFF) as u8;
-        d4[2..].copy_from_slice(node);
-        Uuid::from_fields(time_low, time_mid, time_high_and_ver, &d4)
+        let time_low;
+        let time_mid;
+        let time_high_and_version;
+
+        {
+            /// The number of 100 ns ticks between the UUID epoch
+            /// `1582-10-15 00:00:00` and the Unix epoch `1970-01-01 00:00:00`.
+            const UUID_TICKS_BETWEEN_EPOCHS: u64 = 0x01B2_1DD2_1381_4000;
+
+            let timestamp = seconds * 10_000_000 + u64::from(nano_seconds / 100);
+            let uuid_time = timestamp + UUID_TICKS_BETWEEN_EPOCHS;
+
+            time_low = (uuid_time & 0xFFFF_FFFF) as u32;
+            time_mid = ((uuid_time >> 32) & 0xFFFF) as u16;
+            time_high_and_version = (((uuid_time >> 48) & 0x0FFF) as u16) | (1 << 12);
+        }
+
+        let mut d4 = [0_u8; 8];
+
+        {
+            let count = context.generate_sequence(seconds, nano_seconds);
+            d4[0] = (((count & 0x3F00) >> 8) as u8) | 0x80;
+            d4[1] = (count & 0xFF) as u8;
+        }
+
+        d4[2..].copy_from_slice(node_id);
+
+        Uuid::from_fields(time_low, time_mid, time_high_and_version, &d4)
     }
 }
 
