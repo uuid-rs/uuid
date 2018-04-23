1407687bd40dc7ecde9eb601e62b7359932ddddc
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-04-23 15:32:40 -0700

    fix format
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- benches/parse_str.rs
+++ benches/parse_str.rs
@@ -4,10 +4,11 @@
 extern crate slog;
 extern crate test;
 extern crate uuid;
+
 #[cfg(feature = "slog")]
-use uuid::prelude::*;
 use slog::Drain;
 use test::Bencher;
+use uuid::prelude::*;
 
 #[bench]
 fn bench_parse(b: &mut Bencher) {
