87318be26a0bbf375008c774b7f9072ea72f0f61
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-05-11 16:51:06 -0700

    Add COPYRIGHT file and add license headers to all rs files
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-// Copyright 2013-2014 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
+// Copyright 2013-2014 The Rust Project Developers.
+// Copyright 2018 The Uuid Project Developers.
+//
+// See the COPYRIGHT file at the top-level directory of this distribution.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
