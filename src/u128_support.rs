303f761b74de358a06a1036602acd185b1f1bd48
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-05-20 20:01:31 -0700

    add test for u128
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/u128_support.rs
+++ src/u128_support.rs
@@ -37,3 +37,20 @@ impl From<u128> for Uuid {
         Uuid { bytes }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use prelude::*;
+
+    #[test]
+    fn test_from_u128() {
+        const U128: u128 = 0x3a0724b4_93a0_4d87_ac28_759c6caa13c4;
+
+        let uuid = Uuid::from(U128);
+
+        let uuid2: Uuid = U128.into();
+
+        assert_eq!(uuid, uuid2)
+    }
+
+}
