564662a56491b0bdc8120b4f8309fbc66e4c24c0
Murray J Brown <mjb@mjbrown.com>
2018-09-01 18:21:22 -0400

    #236: refactored benchmarks for parse_str.rs

--- benches/invalid_parse_str.rs
+++ benches/invalid_parse_str.rs
@@ -27,20 +27,6 @@ fn bench_parse(b: &mut Bencher) {
         let _ = Uuid::parse_str("67e5504410b1426f9247bb680e5fe0cg8");
         let _ = Uuid::parse_str("67e5504410b1426%9247bb680e5fe0c8");
 
-        // Valid
-        let _ = Uuid::parse_str("00000000000000000000000000000000");
-        let _ = Uuid::parse_str("67e55044-10b1-426f-9247-bb680e5fe0c8");
-        let _ = Uuid::parse_str("67e55044-10b1-426f-9247-bb680e5fe0c8");
-        let _ = Uuid::parse_str("F9168C5E-CEB2-4faa-B6BF-329BF39FA1E4");
-        let _ = Uuid::parse_str("67e5504410b1426f9247bb680e5fe0c8");
-        let _ = Uuid::parse_str("01020304-1112-2122-3132-414243444546");
-        let _ =
-            Uuid::parse_str("urn:uuid:67e55044-10b1-426f-9247-bb680e5fe0c8");
-
-        // Nil
-        let _ = Uuid::parse_str("00000000000000000000000000000000");
-        let _ = Uuid::parse_str("00000000-0000-0000-0000-000000000000");
-
         // Test error reporting
         let _ = Uuid::parse_str("67e5504410b1426f9247bb680e5fe0c");
         let _ = Uuid::parse_str("67e550X410b1426f9247bb680e5fe0cd");
@@ -74,29 +60,5 @@ fn bench_parse_invalid_group_len(b: &mut Bencher) {
 fn bench_parse_invalid_groups(b: &mut Bencher) {
     b.iter(|| {
         let _ = Uuid::parse_str("F9168C5E-CEB2-4faa-B6BFF329BF39FA1E4");
-    });
-}
-
-#[bench]
-fn bench_valid_hyphenated(b: &mut Bencher) {
-    b.iter(|| {
-        let _ = Uuid::parse_str("67e55044-10b1-426f-9247-bb680e5fe0c8");
-    });
-}
-
-#[bench]
-fn bench_valid_short(b: &mut Bencher) {
-    b.iter(|| {
-        let _ = Uuid::parse_str("67e5504410b1426f9247bb680e5fe0c8");
-    });
-}
-
-#[cfg(feature = "slog")]
-#[bench]
-fn bench_log_discard_kv(b: &mut Bencher) {
-    let root = slog::Logger::root(slog::Discard.fuse(), o!());
-    let u1 = Uuid::parse_str("F9168C5E-CEB2-4FAB-B6BF-329BF39FA1E4").unwrap();
-    b.iter(|| {
-        crit!(root, "test"; "u1" => u1);
     });
 }
