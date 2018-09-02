381ca105dffb88fc2f89b3d0fbdc2193a1575c35
dylan_DPC <dylan.dpc@gmail.com>
2018-09-02 16:22:09 +0530

    after fmt

--- benches/slog_support/parse_str.rs
+++ benches/slog_support/parse_str.rs
@@ -2,8 +2,10 @@ use test::Bencher;
 
 #[bench]
 pub fn bench_log_discard_kv(b: &mut Bencher) {
-    #[cfg(feature = "slog")] {
-        let u1 = Uuid::parse_str("F9168C5E-CEB2-4FAB-B6BF-329BF39FA1E4").unwrap();
+    #[cfg(feature = "slog")]
+    {
+        let u1 =
+            Uuid::parse_str("F9168C5E-CEB2-4FAB-B6BF-329BF39FA1E4").unwrap();
         let root = slog::Logger::root(slog::Discard.fuse(), o!());
     }
     b.iter(|| {
