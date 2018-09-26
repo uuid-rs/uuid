d7ab593b67a114b72d650cb97dfee19fd5232e48
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-09-27 01:24:36 +0900

    fix the value of html_root_url in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -116,7 +116,7 @@
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/uuid"
+    html_root_url = "https://docs.rs/uuid/0.7.1"
 )]
 
 #[cfg(feature = "byteorder")]
