c49f11d10d925bd400410be2b051299a38bec98c
Hunar Roop Kahlon <hunar.roop@gmail.com>
2018-10-08 16:19:05 -0700

    cleanup adapter::encode
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/adapter/mod.rs
+++ src/adapter/mod.rs
@@ -234,8 +234,7 @@ fn encode<'a>(
             // number.
             let hyphens_before = if hyphens { group } else { 0 };
 
-            for idx in BYTE_POSITIONS[group]..BYTE_POSITIONS[group + 1] {
-                let b = bytes[idx];
+            for (idx, b) in bytes.iter().enumerate().take(BYTE_POSITIONS[group + 1]).skip(BYTE_POSITIONS[group]) {
                 let out_idx = hyphens_before + 2 * idx;
 
                 buffer[out_idx] = hex[(b >> 4) as usize];
