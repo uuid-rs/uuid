3fec3105e208f7db182e6dfbb0c0e1d18b0bad0c
Hunar Roop Kahlon <hunar.roop@gmail.com>
2019-06-22 23:20:55 -0700

    from u128 to endian
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/adapter/endian/core_support.rs
+++ src/adapter/endian/core_support.rs
@@ -1,7 +1,26 @@
 use byteorder;
 use crate::prelude::*;
 use crate::adapter::endian;
-use rand::AsByteSliceMut;
+
+impl From<u128> for endian::Big {
+    fn from(f: u128) -> Self {
+        let mut bytes: Bytes = [0; 16];
+
+        <byteorder::BE as byteorder::ByteOrder>::write_u128(&mut bytes, f);
+
+        endian::Big(bytes)
+    }
+}
+
+impl From<u128> for endian::Little {
+    fn from(f: u128) -> Self {
+        let mut bytes: Bytes = [0; 16];
+
+        <byteorder::LE as byteorder::ByteOrder>::write_u128(&mut bytes, f);
+
+        endian::Little(bytes)
+    }
+}
 
 impl From<Uuid> for endian::Big {
     fn from(f: Uuid) -> Self {
