165b7eb28194328a75dc0cd78b5a45b3152cc23f
Ashley Mannix <kodraus@hey.com>
2022-02-28 09:35:32 +1000

    move to sha1_smol as the new name for the current library

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "v5")]
 pub(crate) fn hash(ns: &[u8], src: &[u8]) -> [u8; 16] {
-    use private_sha1::{Sha1, Digest};
+    use private_sha1_smol::Sha1;
 
     let mut hasher = Sha1::new();
 
@@ -8,7 +8,7 @@ pub(crate) fn hash(ns: &[u8], src: &[u8]) -> [u8; 16] {
     hasher.update(src);
 
     let mut bytes = [0; 16];
-    bytes.copy_from_slice(&hasher.finalize()[..16]);
+    bytes.copy_from_slice(&hasher.digest().bytes()[..16]);
 
     bytes
 }
