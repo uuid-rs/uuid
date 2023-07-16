33f6b3edd9ade424ce014dfc808576f657803fa3
KodrAus <kodraus@hey.com>
2023-07-17 08:38:54 +1000

    print uuids in examples

--- examples/random_uuid.rs
+++ examples/random_uuid.rs
@@ -2,14 +2,16 @@
 //!
 //! If you enable the `v4` feature you can generate random UUIDs.
 
-#[test]
 #[cfg(feature = "v4")]
-fn generate_random_uuid() {
+fn main() {
     use uuid::Uuid;
 
     let uuid = Uuid::new_v4();
 
     assert_eq!(Some(uuid::Version::Random), uuid.get_version());
+
+    println!("{}", uuid);
 }
 
+#[cfg(not(feature = "v4"))]
 fn main() {}
--- examples/sortable_uuid.rs
+++ examples/sortable_uuid.rs
@@ -2,14 +2,16 @@
 //!
 //! If you enable the `v7` feature you can generate sortable UUIDs.
 
-#[test]
 #[cfg(feature = "v7")]
-fn generate_sortable_uuid() {
+fn main() {
     use uuid::Uuid;
 
     let uuid = Uuid::now_v7();
 
     assert_eq!(Some(uuid::Version::SortRand), uuid.get_version());
+
+    println!("{}", uuid);
 }
 
+#[cfg(not(feature = "v7"))]
 fn main() {}
