a9236148e95ff63af80b8c610f0e348c8c163986
Shiv Dhar <shivdhar@gmail.com>
2024-02-25 02:08:09 +0530

    add test for multicast bit when making random MAC

--- src/node_id.rs
+++ src/node_id.rs
@@ -14,8 +14,18 @@ fn make_random_node_id() -> [u8; 6] {
 
     crate::rng::fill_random_bytes(&mut rand_bytes);
 
-    // set muticast bit
+    // set multicast bit
     rand_bytes[0] = rand_bytes[0] | 0x01;
-
     rand_bytes
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_multicast_bit_set() {
+        // non deterministic test
+        let node1 = super::make_random_node_id();
+        assert_eq!(node1[0] & 0x01, 1);
+    }
 }
