704421094aeb006f910133e9567368c588474e61
KodrAus <kodraus@hey.com>
2025-02-18 08:05:13 +1000

    prepare for 1.13.2 release

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.13.1"
+//! version = "1.13.2"
 //! features = [
 //!     "v4",                # Lets you generate random UUIDs
 //!     "fast-rng",          # Use a faster (but still sufficiently random) RNG
@@ -140,7 +140,7 @@
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.13.1"
+//! version = "1.13.2"
 //! features = [
 //!     "v4",
 //!     "v7",
@@ -155,7 +155,7 @@
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.13.1"
+//! version = "1.13.2"
 //! default-features = false
 //! ```
 //!
@@ -213,7 +213,7 @@
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/uuid/1.13.1"
+    html_root_url = "https://docs.rs/uuid/1.13.2"
 )]
 
 #[cfg(any(feature = "std", test))]
